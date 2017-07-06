da65a4c2a12a6d188ce4c0d61d9c0633880e6c09
WeiqingYu <weiqingy@umich.edu>
2017-07-06 15:40:41 -0700

    Update example.py

--- example.py
+++ example.py
@@ -11,6 +11,7 @@ import math
 import matplotlib.pyplot as plt
 
 
+# generate random matrix with smooth rows
 base = np.linspace(0,math.pi,40)
 mat_b = np.array([[math.sin(x) for x in base],
                   [math.cos(x) for x in base],
@@ -24,12 +25,16 @@ complete_dat = np.dot(mat_a,mat_b)+np.random.rand(2000,40)
 test_dat = complete_dat.copy()
 test_dat.ravel()[np.random.choice(test_dat.size,16000,replace=False)]=np.nan
 
+# initialize the SAS object 
 recover = SAS(thres=0.00001,rank = 5)
+
+# recover missing entries
 sasres_a, sasres_b = recover.fit(test_dat,complete_dat,True)
 
 sasres = np.dot(sasres_a.transpose(),sasres_b)
 print('MAE of estimation:',np.mean(abs(sasres-complete_dat)[np.isnan(test_dat)]))
 
+# plot the first row of recovered data and ground truth
 fig, ax = plt.subplots()
 plt.plot(test_dat[0],'bs',label = 'Data with Missing Entries')
 plt.plot(complete_dat[0],'g',label = 'Complete Data',linewidth = 3.0)
